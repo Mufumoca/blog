765b9d8ac681f6b5edcc908902dfe18131ebfdad
Mufumoca <1142794187@qq.com>
2026-03-07 18:20:58 +0800

    feat: add Sakim and Oblique friend links

--- src/data/friends.ts
+++ src/data/friends.ts
@@ -11,7 +11,24 @@ export interface FriendItem {
 }
 
 // 友情链接数据
-export const friendsData: FriendItem[] = [];
+export const friendsData: FriendItem[] = [
+	{
+		id: 1,
+		title: "Sakim",
+		imgurl: "https://i.111666.best/image/JlG0wbfpVTFYx3xu3sO64l.jpg",
+		desc: "It's the only NEET thing to do.",
+		siteurl: "https://blog.kokonoyu.com",
+		tags: ["博客"],
+	},
+	{
+		id: 2,
+		title: "Oblique",
+		imgurl: "https://img.aspchang.com/file/1770816588654_avatar.png",
+		desc: "一滩败草 一袖寒风",
+		siteurl: "https://www.aspchang.com",
+		tags: ["博客"],
+	},
+];
 
 // 获取所有友情链接数据
 export function getFriendsList(): FriendItem[] {
